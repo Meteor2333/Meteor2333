fd55b913b6c832d5cee42a78f69014338276f0bd
Meteor2333 <Meteor23333@outlook.com>
2026-08-03 11:58:10 +0800

    fix: format SVG output

--- scripts/generators/metrics.js
+++ scripts/generators/metrics.js
@@ -9,15 +9,15 @@ const xml = parser.parse(output);
 const width = xml.svg["@_width"];
 const height = xml.svg["@_height"];
 // 添加加载后渐亮动画效果
-output = 
-`<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}">
+output = `
+<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}">
 ${output}
-  <animate
-    attributeName="opacity"
-    from="0"
-    to="1"
-    dur="0.3s"
-    fill="freeze"/>
+<animate
+  attributeName="opacity"
+  from="0"
+  to="1"
+  dur="0.3s"
+  fill="freeze"/>
 </svg>
 `;
 writeFileSync('../metrics.svg', output);
